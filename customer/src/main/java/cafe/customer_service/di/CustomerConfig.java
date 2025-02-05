f4315a33a87d7116cbd695578b452b49e4131d47
privalov_ivan <mamamia89678524930@gmail.com>
2025-02-06 01:22:15 +0500

    upd

--- customer/src/main/java/cafe/customer_service/di/CustomerConfig.java
+++ customer/src/main/java/cafe/customer_service/di/CustomerConfig.java
@@ -1,4 +1,19 @@
 package cafe.customer_service.di;
 
-public class CustomerDataInit {
+import cafe.customer_service.model.Customer;
+import cafe.customer_service.repository.CustomerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class CustomerConfig {
+    @Bean
+    public CommandLineRunner init(CustomerRepository customerRepository) {
+        return args -> {
+            customerRepository.save(new Customer(null, "Ivan Ivanov", "ivan@example.com", "123456789"));
+            customerRepository.save(new Customer(null, "Mashka Smirnova", "mashka@example.com", "987654321"));
+            customerRepository.save(new Customer(null, "Pavel Valerich", "pavel@example.com", "777777777"));
+        };
+    }
 }
